4b11af043776b896ed780eb58d69ceddcfb32fab
adhvait7 <adhvait7@gmail.com>
2026-01-05 11:34:03 +0530

    Pipeline complete. data gets stored as CSV now.

---
+++ config.py
@@ -0,0 +1,4 @@
+API_URL = "https://api.coingecko.com/api/v3/simple/price"
+ASSETS = ["bitcoin"]
+OUTPUT_DIR = "data/raw"
+SOURCE = "coingecko"
--- run_pipeline.py
+++ run_pipeline.py
@@ -1,9 +1,9 @@
 from ingestion.fetch_market_data import fetch_market_data
+from transforms.normalize_market_data import normalize_market_data
 
 def main():
     fetch_market_data()
-    
+    normalize_market_data()
+
 if __name__ == "__main__":
     main()
-    
-    
